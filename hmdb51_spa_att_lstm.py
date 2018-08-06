6562341d45df9c3f89198b0415a7c23a4379414d
Lili <lilimeng1103@gmail.com>
2018-08-05 22:02:15 -0700

    fix bugs

--- hmdb51_spa_att_lstm.py
+++ hmdb51_spa_att_lstm.py
@@ -29,6 +29,7 @@ import numpy as np
 import time
 from PIL import Image
 from network import *
+from feature_dataloader import *
 use_cuda = True
 
 class Action_Att_LSTM(nn.Module):
@@ -38,8 +39,9 @@ class Action_Att_LSTM(nn.Module):
 		self.att_vw = nn.Linear(49*2048, 49, bias=False)
 		self.att_hw = nn.Linear(hidden_size, 49, bias=False)
 		self.att_bias = nn.Parameter(torch.zeros(49))
-		self.att_w = nn.Linear(49, 1, bias=False)
-	
+		#self.att_w = nn.Linear(49, 1, bias=False)
+		self.att_vw_bn= nn.BatchNorm1d(49)
+		self.att_hw_bn= nn.BatchNorm1d(49)
 		self.hidden_size = hidden_size
 		self.lstm = nn.LSTM(input_size, hidden_size)
 		self.fc = nn.Linear(hidden_size, output_size)
@@ -61,10 +63,12 @@ class Action_Att_LSTM(nn.Module):
 	  	"""
 	  	features_tmp = features.contiguous().view(batch_size, -1)
 	  	
-	  	att_fea = self.att_vw(features_tmp)  
+	  	att_fea = self.att_vw(features_tmp)
+	  	att_fea = self.att_vw_bn(att_fea)
 	  	att_h = self.att_hw(hiddens)
-	  	#att_out = att_fea + att_h
-	  	att_out = att_h
+	  	att_h = self.att_hw_bn(att_h)
+	  	att_out = att_fea + att_h
+	  	#att_out = att_h
 
 	  	alpha = nn.Softmax()(att_out)
 
@@ -150,10 +154,8 @@ def train(batch_size,
 	loss = 0
 	model_optimizer.zero_grad()
 
-	model_input = (train_data).view(batch_size, -1, FLAGS.num_segments, 49)
+	model_input = (train_data).view(batch_size, -1, FLAGS.num_segments, 49).cuda()
 	
-	model_input = model_input.cuda()
-
 	logits, spa_att_weights = model.forward(model_input)
 
 	loss += criterion(logits, train_label) 
@@ -168,7 +170,7 @@ def train(batch_size,
 
 	train_accuracy = 100.0 * corrects/batch_size
 
-	return final_loss, train_accuracy, spa_att_weights
+	return final_loss, train_accuracy, spa_att_weights, corrects
 
 def test_step(batch_size,
 			 batch_x,
@@ -177,13 +179,13 @@ def test_step(batch_size,
 	
 	test_data_batch = batch_x.view(batch_size, -1, FLAGS.num_segments, 49).cuda()
 
-	test_logits, spa_att_weights = model(test_data_batch)
+	test_logits, spa_att_weights = model.forward(test_data_batch)
 	
 	corrects = (torch.max(test_logits, 1)[1].view(batch_y.size()).data == batch_y.data).sum()
 
 	test_accuracy = 100.0 * corrects/batch_size
 
-	return test_logits, test_accuracy, spa_att_weights
+	return test_logits, test_accuracy, spa_att_weights, corrects
 
 
 def main():
@@ -195,110 +197,77 @@ def main():
 
 	num_segments = FLAGS.num_segments
 
-	train_data = np.load("./saved_features/spa_train_hmdb51_features.npy")
-	train_label = np.load("./saved_features/spa_train_hmdb51_labels.npy")
-	train_name = np.load("./saved_features/spa_train_hmdb51_names.npy")
+	# load train data
+	train_data_dir = './spa_features/train'
+	train_csv_file = './spa_features/train_features_list.csv'
 
-	test_data = np.load("./saved_features/spa_test_hmdb51_features.npy")
-	test_label = np.load("./saved_features/spa_test_hmdb51_labels.npy")
-	test_name = np.load("./saved_features/spa_test_hmdb51_names.npy")
+	train_data_loader = get_loader(data_dir=train_data_dir, 
+							csv_file = train_csv_file, 
+							batch_size = FLAGS.train_batch_size,
+							mode ='train',
+							dataset='hmdb51')
+	# load test data
+	test_data_dir = './spa_features/test'
+	test_csv_file = './spa_features/test_features_list.csv'
+	test_data_loader = get_loader(data_dir = test_data_dir, 
+    						csv_file = test_csv_file, 
+    						batch_size = FLAGS.test_batch_size, 
+    						mode='test',
+                            dataset='hmdb51')
 
-	print("train_data.shape: ", train_data.shape)
-
-	train_data = torch.from_numpy(train_data)
-	train_data = train_data.transpose(1,2)
-	train_label = torch.from_numpy(train_label)
-
-	test_data = torch.from_numpy(test_data)
-	test_data = test_data.transpose(1,2)
-	test_label = torch.from_numpy(test_label)
-	print("train_data.shape: ", train_data.shape)
-
-	transform = transforms.Compose([
-	            #transforms.Scale([224, 224]),
-	            transforms.ToTensor()])
-
-	# replace_with_random_noise_end = False
-	# replace_with_random_noise_middle = False
-
-	# noisy_train = torch.randn(train_data.shape[0], train_data.shape[1], 5)
-	# noisy_test = torch.randn(test_data.shape[0], test_data.shape[1], 5)
-
-	# if replace_with_random_noise_end == True:
-	
-	# 	train_data = torch.cat((train_data[:,:,0:10], noisy_train),2)		
-	# 	test_data = torch.cat((test_data[:,:,0:10], noisy_test), 2)
-	
-	# if	replace_with_random_noise_middle == True:
-
-	# 	train_data1 = torch.cat((train_data[:,:,0:5], noisy_train), 2)
-	# 	train_data = torch.cat((train_data1, train_data[:,:,10:15]),2)
-
-	# 	test_data1 = torch.cat((test_data[:,:,0:5], noisy_test), 2)
-	# 	test_data = torch.cat((test_data1, test_data[:,:,10:15]),2)
-
-	# print("train_data.shape: ", train_data.shape)
-	# print("train_label.shape: ", train_label.shape)
-	# replicate_frames_5_times = False
-
-
-	# if replicate_frames_5_times == True:
-	# 	train_data = np.repeat(train_data, 2, axis=2)
-	# 	train_label = np.repeat(train_label, 2, axis=0)
-	# 	test_data = np.repeat(test_data, 2, axis=2)
-	# 	test_label = np.repeat(test_label, 2, axis=0)
+	category_dict = np.load("./category_dict.npy")
 
 	lstm_action = Action_Att_LSTM(input_size=2048, hidden_size=512, output_size=51, seq_len=FLAGS.num_segments).cuda()
-	model_optimizer = torch.optim.Adam(lstm_action.parameters(), lr=1e-5) 
-	#model_optimizer = torch.optim.Adam(lstm_action.parameters(), lr=1e-3)
+	#model_optimizer = torch.optim.Adam(lstm_action.parameters(), lr=1e-5) 
+	model_optimizer = torch.optim.SGD(lstm_action.parameters(), lr=1e-3)
 	criterion = nn.CrossEntropyLoss()  
 
 	best_test_accuracy = 0
-
-	num_step_per_epoch_train = train_data.shape[0]//FLAGS.train_batch_size
-	num_step_per_epoch_test = test_data.shape[0]//FLAGS.test_batch_size
-
 	
-	log_dir = os.path.join('./new_tensorboard_log', 'nobias_Adam1e-5_spa_att_hidden512_LSoftmaxonlyhidden'+time.strftime("_%b_%d_%H_%M", time.localtime()))
+	log_dir = os.path.join('./51HMDB51_tensorboard', 'nobias_Adam1e-3_spa_att_hiddenCurrent512_LSoftmaxonlyhidden'+time.strftime("_%b_%d_%H_%M", time.localtime()))
 
 	if not os.path.exists(log_dir):
 		os.makedirs(log_dir)
 	writer = SummaryWriter(log_dir)
 
+	num_step_per_epoch_train = 3570/FLAGS.train_batch_size
+	num_step_per_epoch_test = 1530/FLAGS.test_batch_size
 	for epoch_num in range(maxEpoch):
 
-		model_optimizer = lr_scheduler(optimizer = model_optimizer, epoch_num=epoch_num, init_lr = 1e-5, lr_decay_epochs=150)
+		model_optimizer = lr_scheduler(optimizer = model_optimizer, epoch_num=epoch_num, init_lr = 1e-3, lr_decay_epochs=70)
 		
 		lstm_action.train()
 		avg_train_accuracy = 0
-		permutation = torch.randperm(train_data.shape[0])
+
 		train_name_list =[]
 		train_spa_att_weights_list = []
-		for i in range(0, train_data.shape[0], FLAGS.train_batch_size):
-			indices = permutation[i:i+FLAGS.train_batch_size]
-			train_batch_x, train_batch_y, train_batch_name = train_data[indices], train_label[indices], train_name[indices]
-			train_batch_x = Variable(train_batch_x).cuda().float()
-			train_batch_y = Variable(train_batch_y).cuda().long()
-			#print("train_batch_name[0:5] ", train_batch_name[0:5])
-
-			train_loss, train_accuracy, train_spa_att_weights = train(FLAGS.train_batch_size, train_batch_x, train_batch_y, lstm_action, model_optimizer, criterion)
+		total_train_corrects = 0
+		for i, (train_sample,train_batch_name) in enumerate(train_data_loader):
+			train_batch_feature = train_sample['feature']
+			train_batch_label = train_sample['label']
+			train_batch_feature = Variable(train_batch_feature).cuda().float()
+			train_batch_label = Variable(train_batch_label[:,0]).cuda().long()
+			
+			train_loss, train_accuracy, train_spa_att_weights, train_corrects = train(FLAGS.train_batch_size, train_batch_feature, train_batch_label, lstm_action, model_optimizer, criterion)
 			#print("train_spa_att_weights[0:5] ",train_spa_att_weights[0:5])
 			train_name_list.append(train_batch_name)
 			train_spa_att_weights_list.append(train_spa_att_weights)
 			avg_train_accuracy+=train_accuracy
+			print("batch_train_acc: ", train_accuracy)
+			total_train_corrects+= train_corrects
 			
-		
 		train_spa_att_weights_np = torch.cat(train_spa_att_weights_list, dim=0)
-
-		print("train_spa_att_weights_np.shape: ",train_spa_att_weights_np.shape)
-		np.save("./saved_weights/train_name.npy", np.asarray(train_name_list))
-		np.save("./saved_weights/train_att_weights.npy", train_spa_att_weights_np.cpu().data.numpy())
+		avg_train_corects = total_train_corrects *100 /3570
+		#print("train_spa_att_weights_np.shape: ",train_spa_att_weights_np.shape)
+		#np.save("./saved_weights/hc_train_name.npy", np.asarray(train_name_list))
+		#np.save("./saved_weights/hc_train_att_weights.npy", train_spa_att_weights_np.cpu().data.numpy())
 		final_train_accuracy = avg_train_accuracy/num_step_per_epoch_train
 		print("epoch: "+str(epoch_num)+ " train accuracy: " + str(final_train_accuracy))
+		print("epoch: "+str(epoch_num)+ " train corrects: " + str(avg_train_corrects))
 		writer.add_scalar('train_accuracy', final_train_accuracy, epoch_num)
    
 
-		save_train_file = "hid_only"+FLAGS.dataset  + "_numSegments"+str(FLAGS.num_segments)+"_regFactor_"+str(FLAGS.hp_reg_factor)+"_train_acc.txt"
+		save_train_file = "hid_current"+FLAGS.dataset  + "_numSegments"+str(FLAGS.num_segments)+"_regFactor_"+str(FLAGS.hp_reg_factor)+"_train_acc.txt"
 		with open(save_train_file, "a") as text_file:
 				print(f"{str(final_train_accuracy)}", file=text_file)
 
@@ -306,39 +275,45 @@ def main():
 		lstm_action.eval()
 		test_name_list =[]
 		test_spa_att_weights_list = []
-		for i in range(0, test_data.shape[0], FLAGS.test_batch_size):
-			test_indices = range(test_data.shape[0])[i: i+FLAGS.test_batch_size]
-			test_batch_x, test_batch_y, test_batch_name = test_data[test_indices], test_label[test_indices], test_name[test_indices]
-			test_batch_x = Variable(test_batch_x).cuda().float()
-			test_batch_y = Variable(test_batch_y).cuda().long()
+		total_test_corrects = 0
+		for i, (test_sample, test_batch_name) in enumerate(test_data_loader):
+			test_batch_feature = test_sample['feature']
+			test_batch_label = test_sample['label']
+			
+			test_batch_feature = Variable(test_batch_feature, volatile=True).cuda().float()
+			test_batch_label = Variable(test_batch_label[:,0], volatile=True).cuda().long()
 			
 
-			test_logits, test_accuracy, test_spa_att_weights = test_step(FLAGS.test_batch_size, test_batch_x, test_batch_y, lstm_action)
+			test_logits, test_accuracy, test_spa_att_weights, test_corrects = test_step(FLAGS.test_batch_size, test_batch_feature, test_batch_label, lstm_action)
 
 			test_name_list.append(test_batch_name)
 			test_spa_att_weights_list.append(test_spa_att_weights)
-			#print("test_batch_name[0:5]: ", test_batch_name[0:5])
-			#print("test_spa_att_weights[0:5] ",test_spa_att_weights[0:5])
+			
+			print("batch_test_accuracy: ", test_accuracy)
+			total_test_corrects += test_corrects 
+
 			avg_test_accuracy+= test_accuracy
 
-		
+		avg_test_corrects = total_test_corrects*100/1530
+
 		test_spa_att_weights_np = torch.cat(test_spa_att_weights_list, dim=0)
 		print("test_spa_att_weights_np.shape ", test_spa_att_weights_np.shape)
-		np.save("./saved_weights/test_name.npy", np.asarray(test_name_list))
-		np.save("./saved_weights/test_att_weights.npy", test_spa_att_weights_np.cpu().data.numpy())
+		#np.save("./saved_weights/hc_test_name.npy", np.asarray(test_name_list))
+		#np.save("./saved_weights/hc_test_att_weights.npy", test_spa_att_weights_np.cpu().data.numpy())
 	
 		final_test_accuracy = avg_test_accuracy/num_step_per_epoch_test
 		print("epoch: "+str(epoch_num)+ " test accuracy: " + str(final_test_accuracy))
+		print("epoch: "+str(epoch_num)+ " test corrects: " + str(avg_test_corrects))
 		writer.add_scalar('test_accuracy', final_test_accuracy, epoch_num)
 
-		save_test_file = "hid_only"+FLAGS.dataset  + "_numSegments"+str(FLAGS.num_segments)+"_regFactor_"+str(FLAGS.hp_reg_factor)+"_test_acc.txt"
+		save_test_file = "hid_current"+FLAGS.dataset  + "_numSegments"+str(FLAGS.num_segments)+"_regFactor_"+str(FLAGS.hp_reg_factor)+"_test_acc.txt"
 		with open(save_test_file, "a") as text_file1:
 				print(f"{str(final_test_accuracy)}", file=text_file1)
 
 		if final_test_accuracy > best_test_accuracy:
 			best_test_accuracy = final_test_accuracy
 		print('\033[91m' + "best test accuracy is: " +str(best_test_accuracy)+ '\033[0m') 
-
+		
 	# export scalar data to JSON for external processing
 	#writer.export_scalars_to_json("./saved_logs/all_scalars.json")
 	writer.close()
@@ -347,9 +322,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, default='HMDB51',
                         help='dataset: "UCF101", "HMDB51"')
-    parser.add_argument('--train_batch_size', type=int, default=28,
+    parser.add_argument('--train_batch_size', type=int, default=64,
                     	help='train_batch_size: [64]')
-    parser.add_argument('--test_batch_size', type=int, default=30,
+    parser.add_argument('--test_batch_size', type=int, default=64,
                     	help='test_batch_size: [64]')
     parser.add_argument('--max_epoch', type=int, default=200,
                     	help='max number of training epoch: [60]')
